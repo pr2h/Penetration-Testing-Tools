602ce3500fea5340dfcb98073c3e721341680d78
prash <the.prashanthr@gmail.com>
2018-07-18 17:00:59 +0530

    Add files via upload

---
+++ Domain Controller Enumerator/README.md
@@ -0,0 +1,2 @@
+# Domain-Controller-Enumerator
+This bat file can be used to enumerate the DCs in a network
---
+++ Domain Controller Enumerator/dc_enum.bat
@@ -0,0 +1,23 @@
+echo ------------------------------------------------------------------------------------------------- > dc_enum_out.txt
+echo "Computer Domain:" >> dc_enum_out.txt
+systeminfo | findstr /B /C:Domain >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
+echo "User Domain:" >> dc_enum_out.txt
+echo %userdomain% >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
+echo "Computer Domain Name:" >> dc_enum_out.txt
+wmic computersystem get domain >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
+echo "nltest /dclist:%userdomain%" >> dc_enum_out.txt
+nltest /dclist:%userdomain% >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
+echo "LOGONSERVER:" >> dc_enum_out.txt
+echo %LOGONSERVER% >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
+echo "nslookup -type=any %userdnsdomain%" >> dc_enum_out.txt
+nslookup -type=any %userdnsdomain% >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
+echo "_ldap._tcp.dc._msdcs.DOMAIN_NAME" >> dc_enum_out.txt
+set type=all >> dc_enum_out.txt
+_ldap._tcp.dc._msdcs.%userdomain% >> dc_enum_out.txt
+echo ------------------------------------------------------------------------------------------------- >> dc_enum_out.txt
